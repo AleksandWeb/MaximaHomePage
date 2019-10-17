67d73280b733a53b34202ff426fced72d1885a72
Aleksandr Pisarenko <aleksand.web@yandex.ru>
2019-10-17 14:53:56 +0700

    footer is ready 17.10.2019

--- src/js/home.js
+++ src/js/home.js
@@ -71,4 +71,33 @@ $(document).ready(function() {
     /* </submit form> */
 
     /* </Home page weight-form> */
+
+    /* <Walk-men counter> */
+
+    let now_count_meters = 0,
+        now_count_kkal = 0;
+
+    if(localStorage.getItem('now_count_meters')
+        && !isNaN(parseInt(localStorage.getItem('now_count_meters')))) {
+        now_count_meters =  + localStorage.getItem('now_count_meters');
+        now_count_kkal = (now_count_meters * 0.087).toFixed(2)
+    }
+
+    const $countMetrs = $('.walk-men__count-metrs'),
+        $countKkal = $('.walk-men__count-kkal');
+
+    $countMetrs.text(now_count_meters);
+    $countKkal.text(now_count_kkal);
+    setInterval(() => {
+
+        let meters = +$countMetrs.text();
+
+        localStorage.setItem('now_count_meters', (meters + 1).toString());
+
+        $countMetrs.text(localStorage.getItem('now_count_meters'));
+        $countKkal.text((+$countKkal.text() + 0.087).toFixed(2));
+
+    }, 900);
+
+    /* </Walk-men counter> */
 });
